5ec0fcbbb6e309c22635dc9742bbb911e6393eef
Ben Doherty <ben@notionparallax.co.uk>
2016-03-27 11:37:22 -0700

    made it scrape ingredients + refactor

--- getter.py
+++ getter.py
@@ -11,7 +11,7 @@ with open("chefstepsURLS") as f:
 
 
 ff = webdriver.Firefox()
-ff.implicitly_wait(90) # seconds # give it a really long potential timeout in case something strange happens
+ff.implicitly_wait(10) # seconds # give it a really long potential timeout in case something strange happens
 
 def login(username, password):
     ff.get('https://www.chefsteps.com')
@@ -24,25 +24,55 @@ def login(username, password):
     ff.find_element_by_css_selector("button.modal-submit").click()
     time.sleep(3)
     assert 'Home' in ff.title
- 
-login("your@email.address", "yourPassword")
 
-with open("eq.csv", "a") as myfile:
-    myfile.write("title, equipment...\n")
+def make_cell(element):
+    ff.implicitly_wait(0)
+
+    try:
+        name     = element.find_element_by_tag_name('a'   ).text.encode('ascii', 'ignore')
+    except:
+        name     = element.text.encode('ascii', 'ignore')
+
+    # try:
+    #     comments = element.find_element_by_tag_name('span').text.encode('ascii', 'ignore')
+    # except:
+    #     comments = ""
+    ff.implicitly_wait(90)
+
+    return name.replace(",", "|comma|")#, comments.replace(",", "|comma|")
+
+def get_it(selector):
+    row = ""
+    list_things = ff.find_elements_by_css_selector(selector)
+
+    if list_things:
+        for lt in list_things:
+            row += make_cell(lt)+","
+        
+    return row
+
+ 
+login("ben@notionparallax.co.uk", "3Hyd&w%wf86Fe&k4I@3i")
+
+with open("equipment.csv", "w") as equipment_file, open("ingredients.csv", "w") as ingredient_file:
+    equipment_file.write("title, equipment...\n")
     for urlBit in content:
-        row = ""
         ff.get('https://www.chefsteps.com/activities/'+urlBit)
         # assert 'Recipe | ChefSteps' in ff.title
         time.sleep(3)
-        title      = ff.find_elements_by_css_selector('h1')[0]
-        equipment = ff.find_elements_by_css_selector('.activity-amounts-equipment div')  
-        
-        row += ( title.text.encode('ascii', 'ignore') + ",")
-        
-        for e in equipment:
-            row += ( e.text.encode('ascii', 'ignore') + ",")
-        
-    	myfile.write(row + "\n")
-        print row
 
+        #get the title of the page
+        title = ff.find_elements_by_css_selector('h1')[0].text.encode('ascii', 'ignore') 
+        title = title.replace(",", "|comma|")
+        
+        i_row = title + "," + get_it('.ingredient-title-desc')          + "\n" # ingredients
+        e_row = title + "," + get_it('.activity-amounts-equipment div') + "\n" # equipment
+    	equipment_file.write(  e_row )
+        ingredient_file.write( i_row )
+        print "\n",title
+        print i_row
+        print e_row
+
+equipment_file.close()
+ingredient_file.close()
 ff.quit()

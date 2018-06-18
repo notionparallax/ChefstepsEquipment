5cca29feba43a09291b9ce81b2bb36151c1c6316
Ben <ben@notionparallax.co.uk>
2018-06-17 21:06:49 -0700

    minimal overall docstring

--- getter.py
+++ getter.py
@@ -1,3 +1,4 @@
+"""Get ingredients and equipment from the ChefSteps recipes."""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
